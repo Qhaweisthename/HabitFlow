545555f581640618711dbd27644a97ee1566c717
Caleb Hartslief <93492844+Hartslief@users.noreply.github.com>
2025-11-17 18:10:29 +0200

    Updated Login, Added SSO login, Added Biometric login

--- app/src/main/java/com/example/habitflow/App.kt
+++ app/src/main/java/com/example/habitflow/App.kt
@@ -5,10 +5,15 @@ import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
 import androidx.lifecycle.ProcessLifecycleOwner
 import com.example.habitflow.util.AppUsageTracker
+import com.google.firebase.FirebaseApp
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
+
+        // ✅ Initialize Firebase
+        FirebaseApp.initializeApp(this)
+
         ProcessLifecycleOwner.get().lifecycle.addObserver(
             LifecycleEventObserver { _, event ->
                 when (event) {
